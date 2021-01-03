a77368cd2e1e77152b3f0b10a379688c5a9b11b7
Igor <igoreusttaquio@gmail.com>
2021-01-03 18:54:58 +0000

    Return database registers wich json

--- service.php
+++ service.php
@@ -2,4 +2,4 @@
 require_once 'Class/Database.php';
 require_once 'Class/Produto.php';
 
-echo "<pre>".Produto::all(Database::getConnection())."</pre>";
+echo Produto::all(Database::getConnection());
