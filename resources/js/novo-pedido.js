1afa8f23920084ed2cd56754bf779c0b45e406bd
Igor <igoreusttaquio@gmail.com>
2021-01-03 18:54:05 +0000

    Feature, options selet has ben implemented

--- resources/js/novo-pedido.js
+++ resources/js/novo-pedido.js
@@ -1,9 +1,12 @@
 ; (function () {
 
+    
 
     // Preencher LI
     const ul = document.getElementById("ul-produtos");
     const selectProduto = document.getElementById("select-produto");
+    createSelects();
+
     const produtos = selectProduto.getElementsByTagName("option");
 
     const quantidadeInput = document.getElementById("quantidade");
@@ -70,5 +73,25 @@
         spanQuantidade = parseFloat(spanQuantidade);
         totalGeral.value = (parseFloat(totalGeral.value) - (spanQuantidade * parseFloat(spanValue))).toFixed(2);
         this.parentElement.remove();
+    }
+
+    function getDataProduct() {
+        let url = 'http://192.168.1.4/pedidos/service.php';
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", url, false);
+        xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+
+        let jsonParse = JSON.parse(xhttp.responseText);
+        return jsonParse;
+    }
+
+    function createSelects() {
+        [...getDataProduct()].forEach(obj => {
+            let option = document.createElement("option");
+            option.setAttribute("value", obj.codigo);
+            option.textContent = obj.descricao;
+            selectProduto.appendChild(option);
+            console.log(option);
+        }) 
     }
 })()
